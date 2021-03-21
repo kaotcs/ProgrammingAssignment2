b786ebd319f8eda1d19e21e12c3a9e5217dd5218
kaotcs <kaotcs@gmail.com>
2021-03-21 17:53:22 -0300

    Commit my assignment from week 3

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve #set the solve function
+  getsolve <- function() m #save the inverse, after calculated
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)#list where every attributes are saved as a list
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+  m <- x$getsolve() #verify if this inverse has been calculated
+  if(!is.null(m)) { #if it is not null, show "getting cached data" with the inverse
+    message("getting cached data")
+    return(m) #print the inverse matrix in cache
+  }
+  data <- x$get() #get the matrix and store into data
+  m <- solve(data, ...) #m gets the inverse matrix
+  x$setsolve(m) #x receives the inverse of matrix, if the process need to be showed again
+  m #print the inverse matrix
+  }
